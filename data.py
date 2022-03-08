98e11279a5c6fbd6027f349e35a499af16f529ff
santiago <sasouto89@gmail.com>
2022-03-08 19:54:04 +0100

    Modificimos la funcion obtaion_data

--- data.py
+++ data.py
@@ -2,12 +2,12 @@ import csv
 
 class data:
 
-    def __init__(self, station, data_type, data_type_study, value):
+    def __init__(self, station,data_type):
         self.station = station
         self.data_type = data_type
         self.stations_dictionary = {}
-        self.data_type_study = data_type_study
-        self.value = value
+
+        self.data = []
         self.year_matrix = ['1993', '1992', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003',
                        '2004', '2005', '2006', '2007', '2008',
                        '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020',
@@ -26,29 +26,20 @@ class data:
         with open(file_path, encoding="utf8", errors='ignore') as File:
             reader = csv.reader(File)
             #Se selecciona estudio de temperatura media
-            if self.data_type == 0:
-                temperatures = []
-                date = []
-                years = []
-                for row in reader:
-                    if float(row[5]) >= self.value:
-                        temperatures.append(row[self.data_type])
-                        date.append(row[0])
+            if self.data_type == 0: colum = 5
+            if self.data_type == 1: colum = 9
+            if self.data_type == 2: colum = 6
+            if self.data_type == 3: colum = 16
+            for row in reader:
+                    self.data.append({row[colum],row[0]})
 
-                for d in date:
-                    year = d.split('-')
-                    years.append(year[0])
-
-            for a in self.year_matrix:
-                c = years.count(a)
-                print('Año ' + a + ': ' + str(c) + ' días con temperatura media superior a ' + str(self.value) + ' ºC')
-
-    @fill_stations_dictionary
-    def data_type_study(self):
-        print("")
     @fill_stations_dictionary
     def get_stations_dictionary(self): return self.stations_dictionary
-    def set_temperature(self, temperature): self.temperature = temperature
+    def get_data(self):return self.data
 
-a = data('1387E',0,0,18)
-a.obtain_data()
+T_Media = data('1387E',0)
+T_Max = data('1387E',1)
+T_Max.obtain_data()
+T_Media.obtain_data()
+print(T_Max.get_data())
+print(T_Media.get_data())
