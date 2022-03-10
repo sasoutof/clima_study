0d5958745153723b86018aa29e5e542258488797
santiago <sasouto89@gmail.com>
2022-03-10 23:21:41 +0100

    Modificimos la funcion obtaion_data

--- d.py
+++ d.py
@@ -1,17 +1,6 @@
-import math
-class Point:
-    def __init__(self, initX, initY ):
-        self.x = initX
-        self.y = initY
-        def getX(self):return self.x
-        def getY(self):return self.y
-        def distanciaDOrigen(self):
-            return math.sqrt((self.x ** 2) + (self.y ** 2))
-        def distancia(self, point2):
-            xdiff = point2.getX()-self.getX()
-            ydiff = point2.getY()-self.getY()
-            dist = math.sqrt(xdiff**2 + ydiff**2)
-            return dist
-p = Point(7,6)
-q = Point(4,3)
-print(p.distancia(q))
+import re
+
+
+
+x = re.findall("[0-5]", "2.1")
+print(x)
--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 class data:
 
@@ -6,8 +7,8 @@ class data:
         self.station = station
         self.data_type = data_type
         self.stations_dictionary = {}
-
         self.data = []
+        self.data_out = []
         self.year_matrix = ['1993', '1992', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003',
                        '2004', '2005', '2006', '2007', '2008',
                        '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020',
@@ -30,16 +31,42 @@ class data:
             if self.data_type == 1: colum = 9
             if self.data_type == 2: colum = 6
             if self.data_type == 3: colum = 16
-            for row in reader:
-                    self.data.append({row[colum],row[0]})
-
+            for row in reader:self.data.append([row[colum],row[0]])
+    def analyze_data(self, type_of_analyze, valor):
+        self.type_of_analyze = type_of_analyze
+        self.valor = valor
+        if self.type_of_analyze == 0:
+            for row in self.data:
+                #print(re.findall("[^0123456789.]", str(row[0])))
+                if self.valor <= float(row[0]):
+                    self.data_out.append(row[0])
     @fill_stations_dictionary
     def get_stations_dictionary(self): return self.stations_dictionary
     def get_data(self):return self.data
+    def get_data_out(self):return self.data_out
 
-T_Media = data('1387E',0)
-T_Max = data('1387E',1)
-T_Max.obtain_data()
-T_Media.obtain_data()
-print(T_Max.get_data())
-print(T_Media.get_data())
+t_max_1351 = data('1351',0)
+t_max_1387 = data('1387',0)
+t_max_1387E = data('1387E',0)
+t_max_1393 = data('1393',0)
+t_max_1400 = data('1400',0)
+
+t_max_1351.obtain_data()
+t_max_1387.obtain_data()
+t_max_1387E.obtain_data()
+t_max_1393.obtain_data()
+t_max_1400.obtain_data()
+
+
+t_max_1351.analyze_data(0,21)
+t_max_1387.analyze_data(0,21)
+t_max_1387E.analyze_data(0,21)
+t_max_1393.analyze_data(0,21)
+t_max_1400.analyze_data(0,21)
+
+
+print(t_max_1351.get_data())
+print(t_max_1387.get_data())
+print(t_max_1387E.get_data())
+print(t_max_1393.get_data())
+print(t_max_1400.get_data())
